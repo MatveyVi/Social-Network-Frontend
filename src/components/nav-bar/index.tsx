202e9f0a658560e40c78a125f9fd3a54dfae2ba5
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2025-04-08 22:11:03 +0300

    components + nav bar start

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { NavButton } from '../nav-button'
+import { BsPostcard } from 'react-icons/bs'
 
 export const NavBar = () => {
   return (
     <nav>
         <ul className="flex flex-col gap-5">
             <li>
-                <NavButton />
+                <NavButton href='/' icon={<BsPostcard />}>
+                  Посты 
+                </NavButton>
+            </li>
+            <li>
+                <NavButton href='/' icon={<BsPostcard />}>
+                  Посты 
+                </NavButton>
+            </li>
+            <li>
+                <NavButton href='/' icon={<BsPostcard />}>
+                  Посты 
+                </NavButton>
             </li>
         </ul>
     </nav>
